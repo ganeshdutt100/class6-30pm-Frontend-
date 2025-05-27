357efdd07e8b322d44e171d55c92141a82bf2501
Ganesh Dutt <gdutt2304@gmail.com>
2025-05-27 18:57:07 +0530

    JS

---
+++ JavaScript/code1.js
@@ -0,0 +1,10 @@
+function sum() {
+  a = 10;
+  b = 20;
+  return alert(a + b);
+}
+// function sum() {
+//   a = Number(prompt("Enter your first number:"));
+//   b = Number(prompt("Enter your second number:"));
+//   return alert(a + b);
+// }
