4f2bdb2cd49c98fb3cfac14c69d05f9a1e5a6d74
Ganesh Dutt <gdutt2304@gmail.com>
2025-05-28 19:28:17 +0530

    Data Type

---
+++ JavaScript/js/code3.js
@@ -0,0 +1,49 @@
+document.writeln("code 3");
+
+// let nullValue = null;
+// let value;
+
+// console.log(nullValue);
+
+// let a = 43928382949384838483483984839849n;
+// console.log(a);
+
+// Number;
+// null;
+
+// String;
+// Symbol;
+
+// Boolean;
+// BigInt;
+
+// undefined;
+
+// let personName = "Ganesh Dutt";
+
+// Object
+// let person = {
+//   name: "john",
+//   age: 24,
+// };
+
+// console.log(person.name);
+// console.log(person.age);
+
+// 2 Array
+// let arr = [3, 5, "Ganesh Dutt"];
+
+// console.log(arr);
+
+// 3 function
+// function fun(a, b) {
+//   return console.log(a + b);
+// }
+// fun(10, 67);
+
+// let currentDate = new Date();
+// console.log(currentDate);
+
+// let value;
+
+// let value2 = null;
