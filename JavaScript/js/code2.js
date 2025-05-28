ba3bb85a92937906643f544161ce23336d00500e
Ganesh Dutt <gdutt2304@gmail.com>
2025-05-28 19:03:26 +0530

    operators

---
+++ JavaScript/js/code2.js
@@ -0,0 +1,73 @@
+document.writeln("Code2");
+
+let a = 30;
+let b = "30";
+
+// 4. Assignment Operators:
+
+// a = a + 5;
+a += 5;
+a -= 5;
+a *= 5;
+a /= 5; // a = a /5
+console.log(a);
+
+// 3 Logical Operator
+// a = true;
+// b = false;
+
+// and &&
+// true | false =  false
+// true | true =  true
+// false | true =  false
+// false | false =  false
+
+// or ||
+
+// true | false =  true
+// true | true =  true
+// false | true =  true
+// false | false =  false
+// NOT !
+// true  = !(true) => false
+
+// (true) = !(true) => false
+
+// s1 = a;
+// s2 = b;
+//  6:30pm
+
+// a  true
+// b = flase
+
+// && => false
+// || => ture
+// (a) => !(a) => false
+
+// 2 . Relational Operators:
+// console.log(a == b);
+// console.log(a != b);
+// console.log(a > b);
+// console.log(a < b);
+// console.log(a >= b);
+// console.log(a <= b);
+// console.log(a === b);
+
+// 1. Arithmetic Operators:
+// let c = a + b;
+// let c = a - b;
+// let c = a / b;
+// let c = a % b;
+// console.log(c);
+// console.log(a);
+
+// a++;
+// a--;
+// console.log(a);
+// a++;
+// ++a;
+// console.log(a);
+
+// console.log(a++);
+// console.log(a);
+// console.log(++a);
