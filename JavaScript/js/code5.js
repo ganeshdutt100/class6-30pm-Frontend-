22adc58150d0b422d7765b2543f752c47c959281
Ganesh Dutt <gdutt2304@gmail.com>
2025-05-29 18:54:41 +0530

    Update Decision making Statement

---
+++ JavaScript/js/code5.js
@@ -0,0 +1,53 @@
+document.writeln("Code 5");
+
+// let a = 50;
+// let str = "Ganesh Dutt";
+
+// console.log(typeof str);
+
+// if(condition){
+//     stm (true)
+// }
+// else{
+//     stm(false)
+// }
+
+// let a = 99;
+
+//========== 1 ) Simple if statement:
+// if (a % 2 == 0) {
+//   console.log("Even");
+// }
+
+//========= 2) else if statement:
+// if (a % 2 == 0) {
+//   console.log("Even");
+// } else {
+//   console.log("Odd");
+// }
+
+// if-else if-else
+// if(condition){
+//     stm
+// }
+// else if(condition){
+//  stm
+// }
+// .
+// .
+// else{
+//     stm
+// }
+
+// let marks = 60;
+// let marks = prompt("Enter your marks here...");
+
+// if (marks >= 60) {
+//   console.log("first division");
+// } else if (marks >= 40) {
+//   console.log("second division");
+// } else if (marks >= 30) {
+//   console.log("third division");
+// } else {
+//   console.log("fail");
+// }
