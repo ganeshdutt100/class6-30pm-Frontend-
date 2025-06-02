6ace12ee6b7e2cef554d5fef026421c9b9f295f0
Ganesh Dutt <gdutt2304@gmail.com>
2025-06-02 19:00:09 +0530

    Update Funciton

---
+++ JavaScript/js/code6.js
@@ -0,0 +1,44 @@
+// document.writeln("Code 6");
+// Declarations----------------
+// function myFunction() {
+//   let a = 20;
+//   let b = 30;
+//   console.log(a + b);
+// }
+// myFunction();
+
+// function fun(a, b) {
+//   return console.log(a + b);
+// }
+
+// fun(10, 34);
+// fun(10, 98);
+
+// expression-------------------
+// let output = function () {
+//   console.log("Hello world");
+// };
+
+// output();
+
+// let userValue = function() {
+//   let input = prompt("Enter your name ");
+//   console.log(input);
+// };
+
+// userValue();
+
+// IIFE--------------------
+
+// (function () {
+//   console.log("i am IIFE function");
+// })();
+
+// Arrow Function
+
+// let arrowFun = () => console.log("Hello world");
+// let arrowFun = () => {
+//   console.log("Hello world");
+//   console.log("Hello world 2");
+// };
+// arrowFun();
