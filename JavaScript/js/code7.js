9b2031dde45d61a5a5ad0d61ffeba261a1e421e3
Ganesh Dutt <gdutt2304@gmail.com>
2025-06-03 19:03:19 +0530

    Apply

--- JavaScript/js/code7.js
+++ JavaScript/js/code7.js
@@ -2,18 +2,21 @@ document.writeln("Apply method");
 
 // apply("null" , arr)
 
-let a = [23, 56, 78, 90, 34, 23];
+// let a = [23, 56, 78, 90, 34, 23];
 
-let max = Math.max.apply(this, a);
-console.log(max);
-let min = Math.min.apply(this, a);
-console.log(min);
+// let max = Math.max.apply(this, a);
+// console.log(max);
+// let min = Math.min.apply(this, a);
+// console.log(min);
 
-// const students = {
-
-// }
+// const student = {
+//   fullName: function (city, country) {
+//     return this.name + " " + this.last + " from  " + city + " , " + country;
+//   },
+// };
 
 // const person = {
-//   name: "John",
-//   age: 30,
+//   name: "Ganesh",
+//   last: "Dutt",
 // };
+// console.log(student.fullName.apply(person, ["mathura", "India"]));
