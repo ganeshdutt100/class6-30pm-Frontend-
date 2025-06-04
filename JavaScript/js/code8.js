c2b9e219ee48fa0ea880ec72500395944be1c1e7
Ganesh Dutt <gdutt2304@gmail.com>
2025-06-04 19:10:43 +0530

    Update LOOPS

--- JavaScript/js/code8.js
+++ JavaScript/js/code8.js
@@ -8,11 +8,14 @@ document.writeln("code 8");
 // for (let i = 1; i <= 10; i++) {
 //   console.log(i + " Ganesh Dutt");
 // }
+// document.writeln("<br>");
 
 // let table = 5;
 
 // for (let i = 1; i <= 10; i++) {
-//   console.log(table + " * " + i + " = " + table * i);
+//   document.writeln(table + " * " + i + " = " + table * i + "<br>");
+
+//   //   console.log();
 // }
 
 //  table * i = tbale*i
@@ -28,3 +31,35 @@ document.writeln("code 8");
 // .
 // .
 // .
+
+// while
+// let i = 0;
+// while (i <= 10) {
+//   console.log(i);
+//   i++;
+// }
+
+// Do While Loop
+
+// let i = 20;
+// do {
+//   console.log(i);
+//   i++;
+// } while (i <= 10);
+
+// let a = [12, 35, 678, 90];
+// // console.log(a);
+// for (let x of a) {
+//   console.log(x + 10);
+// }
+
+let obj = {
+  name: "Ganesh Dutt",
+  address: "Mathura",
+};
+
+// console.log(obj.address);
+
+for (let x in obj) {
+  console.log(x + " : " + obj[x]);
+}
