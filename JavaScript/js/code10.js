ca2a12b46a9dd72f643805fb4f035b0ecad24aee
unknown <gdutt2304@gmail.com>
2025-06-11 19:42:39 +0530

    Array methods

---
+++ JavaScript/js/code10.js
@@ -0,0 +1,59 @@
+// let a = [12, 34, 5, 6, 7, 8];
+// console.log(a);
+
+// for (let value of a) {
+//   console.log(value);
+// }
+
+// let a1 = [];
+// a1[0] = 34;
+// a1[1] = 34;
+// a1[2] = 34;
+// console.log(a1);
+
+// Array Constructor------------------:
+// let arrValue = new Array();
+// arrValue[0] = 34;
+// arrValue[1] = 14;
+// arrValue[2] = 34;
+// console.log(arrValue);
+
+// let cars = new Array("XUV", "Swift", "Creta");
+
+// console.log(cars);
+
+// MUTATOR Methods
+// let arr = [12, 10, 18, 45, 87, 45, 67, 78];
+// let newArr = [1, 5, 8, 3, 2, 5, 9, 6];
+// newArr.sort();
+// console.log(newArr);
+
+// console.log(arr);
+// fill(value , start , end )
+// n - 1;
+// 5 - 1;
+//  4
+
+// arr.fill("javaScript", 2, 5);
+// arr.pop();
+// arr.push(56);
+// arr.shift()
+// arr.unshift(9);
+// arr.reverse();
+// arr.sort();
+// arr.splice(1, 2, "JavaScript", "React", "Node");
+
+// console.log(arr);
+
+// 2. Accessor Methods: --------------------
+let arr = [12, 10, 18, 45, 87, 45, 67, 78];
+// let newArr = [1, 5, 8, 3, 2, 5];
+
+// let output = arr.concat(newArr);
+// let output = arr.includes(45);
+// let output = arr.indexOf(45, 4);
+// let output = arr.lastIndexOf(45);
+// let output = arr.join(" ,  ");
+// let output = arr.slice(2, 5);
+let output = arr.toString();
+console.log(output);
