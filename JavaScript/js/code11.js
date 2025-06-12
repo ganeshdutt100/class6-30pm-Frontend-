8a8c484c0fc20913ec19a1474c79bb15ea6fa509
unknown <gdutt2304@gmail.com>
2025-06-12 18:03:30 +0530

    Update Array methdos

---
+++ JavaScript/js/code11.js
@@ -0,0 +1,29 @@
+console.log("Code _ 11");
+
+let arr = [12, 45, 67, 8, 9, 45, 67, 4, 8, 2];
+
+// for (let x of arr.entries()) {
+//   console.log(x);
+// }
+
+// let output = arr.every((x) => x % 2 === 0);
+// let output = arr.some((x) => x % 2 != 0);
+// let output = arr.filter((x) => x % 2 != 0);
+// let output = arr.find((x) => x % 2 != 0);
+// let output = arr.findIndex((x) => x % 2 != 0);
+// let output = arr.map((x) => x + 2);
+// let output = arr.reduce((a, b) => a + b);
+// console.log(output);
+
+// let sum = 0;
+// for (let x of arr) {
+//   sum += x; // sum  = sum + x
+// }
+// console.log(sum);
+
+for (let x of arr.keys()) {
+  console.log(x);
+}
+for (let x of arr.values()) {
+  console.log(x);
+}
