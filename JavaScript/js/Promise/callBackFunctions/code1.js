d1d9a6faf92c401729c0a66c9d4ccaf2719b36eb
unknown <gdutt2304@gmail.com>
2025-06-30 17:59:02 +0530

    Update callback function and promise

---
+++ JavaScript/js/Promise/callBackFunctions/code1.js
@@ -0,0 +1,32 @@
+// function greet(name ,callback , callback1){
+//   console.log("Hey "+ name );
+//   callback();
+//   callback1();
+  
+// }
+// function sayGoodMorning(){
+//     console.log("Good_morning!")
+// }
+// function sayBye(){
+//     console.log("Bye")
+// }
+// greet("Ganesh " ,sayGoodMorning , sayBye)
+
+
+// function greet(name, callback){
+//   console.log("Hey "+ name );
+//   callback();
+  
+// }
+//  function combindMessage(){
+//     sayGoodMorning();
+//     sayBye();
+//  }
+
+// function sayGoodMorning(){
+//     console.log("Good_morning!")
+// }
+// function sayBye(){
+//     console.log("Bye")
+// }
+// greet("Ganesh " , combindMessage)
