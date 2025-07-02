2582ebbf16273b1d2e493e7c231150b621c28129
unknown <gdutt2304@gmail.com>
2025-07-02 18:12:34 +0530

    Update Jokes generator

---
+++ JavaScript/js/Promise/project1/codd1.js
@@ -0,0 +1,8 @@
+let values  = ["value1" , "Value 2" , "value 3","value4"]
+
+  let  randomIndex =  Math.floor( Math.random()*values.length)
+  console.log(values[randomIndex]);
+  
+
+//    console.log(values);
+//    0.8 * 3  = 2
